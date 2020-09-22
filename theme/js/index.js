c127fc8cda9615d143f11161c01ac76b0a925b23
naschidaniel <naschidaniel@users.noreply.github.com>
2020-09-22 14:52:35 +0000

    deploy: 0030b2a7cb0171403453b81386ddeeb3f052ba3f

---
+++ theme/js/index.js
@@ -0,0 +1,18 @@
+import "../css/styles.css"
+
+// toggle the menu
+document.getElementById("nav-toggle").onclick = function(){
+  document.getElementById("nav-content").classList.toggle("hidden");
+  document.getElementById("nav-button-open").classList.toggle("hidden");
+  document.getElementById("nav-button-close").classList.toggle("hidden");
+  }
+
+  // close menu on window width >= 640
+  function reportWindowSize() {
+    if (window.innerWidth >= 640) {
+      document.getElementById("nav-content").classList.add("hidden");
+      document.getElementById("nav-button-close").classList.add("hidden");
+      document.getElementById("nav-button-open").classList.remove("hidden");
+    }
+  }
+  window.onresize = reportWindowSize;
