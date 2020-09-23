7482576503e980fccd430fd0d339641fecef08a2
Daniel Naschberger <daniel@naschi.at>
2020-09-23 20:05:19 +0200

    add imagemin

--- imagemin.js
+++ imagemin.js
@@ -2,23 +2,15 @@ const imagemin = require("imagemin-keep-folder");
 const imageminPngquant = require("imagemin-pngquant");
 const imageminMozjpeg = require("imagemin-mozjpeg");
 
-(async () => {
-  await imagemin(["output/**/*.{jpg,png}"], {})
-    .catch(() => {
-      console.error;
-      console.log("There were problems with post-processing the images.")
-      process.exit(1);
-    })
-    .then((files) => {
-      console.log(files);
-      use: [
-        imageminMozjpeg({
-          quality: [0.7, 0.8],
-        }),
-        imageminPngquant({
-          quality: [0.7, 0.8],
-        }),
-      ];
-    });
-  console.log("All images were successfully resized");
-})();
+imagemin(['output/**/*.{jpg,png}'], {
+  use: [
+    imageminMozjpeg({
+      quality: 60,
+    }),
+    imageminPngquant({
+      quality: [0.7, 0.8],
+    }),
+  ],
+}).then(files => { 
+  console.log(files);
+});
